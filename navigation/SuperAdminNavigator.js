c7be034809de52e0d33d159b25be27c13cee42dd
starshieldpaints <146586827+starshieldpaints@users.noreply.github.com>
2025-07-08 00:38:50 +0530

    Add missing navigators and fix imports

---
+++ navigation/SuperAdminNavigator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SuperAdminDashboard from '../screens/Dashboard/SupeAdminDashboard';
+import TaskDetailScreen from '../screens/Tasks/TaskDetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function SuperAdminNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="SuperAdminDashboard" component={SuperAdminDashboard} />
+      <Stack.Screen name="TaskDetail" component={TaskDetailScreen} />
+    </Stack.Navigator>
+  );
+}
